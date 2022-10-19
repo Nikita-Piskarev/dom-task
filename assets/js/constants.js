b5d9b9ef75e5e2e99268748039cc2e37fcf6668d
Nikita Piskarev <npiskarev23@gmail.com>
2022-10-20 01:29:51 +0300

    initial commit

---
+++ assets/js/constants.js
@@ -0,0 +1,25 @@
+const CONSTANTS = {
+  USER_DATA: {
+    DESCRIPTION: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae
+    aut placeat ullam beatae expedita et sapiente quaerat?
+    Laudantium cupiditate beatae placeat, iste ab aliquam facere ex
+    expedita porro, ipsa repellendus minus, illo a. Debitis expedita
+    a ut hic soluta necessitatibus?`,
+    DEFAULT_NAME: "Undefined",
+  },
+  MAX_FRIENDS: 5000,
+};
+
+/*
+ const link = new URL ('https://www.facebook.com/JasonStatham/');
+
+ const SOC_NETWORKS_MAP = new Map();
+
+*/
+
+const imgIcon = new Map([
+  ["facebook", 'assets/images/icons8-facebook-24.png'],
+  ["instagram", "assets/images/icons8-instagram-24.png"],
+  ["twitter", "assets/images/icons8-twitter-24.png"],
+]);
+console.log(imgIcon);
---
+++ assets/js/listeners/imageListeners.js
@@ -0,0 +1,4 @@
+function handleImageErrorV1 (e) {
+  e.target.src = `assets/images/unknownUser.webp`;
+}
+
