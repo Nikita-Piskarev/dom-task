cb1d37d1abce60039fadde2a0097aac25cd05b8c
Nikita Piskarev <npiskarev23@gmail.com>
2022-11-01 00:16:27 +0200

    Add div, ul, function li commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,13 +7,14 @@ const userCards = responseData.map((userData) => createUserCard(userData));
 const workersList = document.querySelector("#root");
 
 workersList.append(...userCards);
+
 /**
  *
  * @param {object} user
  * @returns {HTMLElement}
  */
 function createUserCard(user) {
-  const { firstName, lastName, profilePicture } = user;
+  const { firstName, lastName, profilePicture, contacts } = user;
 
   const fullName =
     !firstName && !lastName ? DEFAULT_NAME : `${firstName} ${lastName}`;
@@ -23,118 +24,27 @@ function createUserCard(user) {
     profilePicture,
   });
 
-  /*
-    Созадать функцию, которая бы подчистила процесс создания 
-    cardInfo 
-    за пример взять createImage
-  */
   const cardInfo = createCardInfo(fullName);
+
+  const linksList = createLinksContainer(contacts);;
+
+  const linksContainer = createElement(
+    "div",
+    { className: "linksContainer" },
+    linksList
+  );
 
   const article = createElement(
     "article",
     { className: "workerCard" },
     imgWrapper,
-    cardInfo
+    cardInfo,
+    linksContainer
   );
 
   return createElement("li", { className: "workerItem" }, article);
 }
 
-function createCardInfo(fullName) {
-  const cardName = createElement("h1", { className: "cardName" });
-  cardName.textContent = fullName;
-
-  const cardDescription = createElement("p", {
-    className: "cardDescription",
-  });
-
-  cardDescription.textContent = DESCRIPTION;
-
-  return createElement(
-    "div",
-    { className: "cardInfo" },
-    cardName,
-    cardDescription
-  );
-}
-
-function createImage(data) {
-  const { fullName, profilePicture } = data;
-
-  const initialsText =
-    fullName === DEFAULT_NAME ? fullName : getInitials(fullName);
-  const initials = createElement("p", { className: "initials" }, initialsText);
-
-  const img = createElement("img", {
-    className: "cardImg",
-    eventListeners: {
-      error: deleteHandler,
-    },
-    attributes: {
-      alt: fullName,
-      src: profilePicture,
-    },
-  });
-
-  return createElement("div", { className: "imgWrapper" }, initials, img);
-}
-
 function deleteHandler(e) {
   e.target.remove();
-}
-
-/**
- * Создает HTML и настраивает Ээемент
- * @param {string} tagName тег создаваемого элемента
- * @param {object} options обьект настроек для элемента
- * @param {string} options.className классы для элемента
- * @param {object} options.eventListeners обьект с функциями - слушателями
- * @param {object} options.attributes обьект атрибутами
- * @param  {...(Element | Node | string)} children - дочерние элементы, узлы или текст
- * @returns {HTMLElement}
- */
-function createElement(tagName, options = {}, ...children) {
-  const { className, eventListeners = {}, attributes = {} } = options;
-
-  const elem = document.createElement(tagName);
-
-  elem.className = className;
-
-  /*
-  eventListeners
-  {
-    'error': handleImageErrorV2,
-    'click' : function(e) {
-    }
-  }
-
-  [
-    [
-      'error',
-      handleImageErrorV2
-    ],
-    [
-      'click',
-      function(e) {}
-    ]
-  ]
-  */
-  for (const [eventType, listener] of Object.entries(eventListeners)) {
-    elem.addEventListener(eventType, listener);
-  }
-  /*
-  attributes 
-  {
-    'src' :'string',
-    'alt': 'string alt'
-  }
-  */
-
-  for (const [attrName, value] of Object.entries(attributes)) {
-    elem.setAttribute(attrName, value);
-  }
-
-  elem.append(...children);
-
-  return elem;
 }
